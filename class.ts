7bd4db5199dbce6cf428583c0726a6436c17973d
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-04 21:53:28 +0600

    inheritance

--- class.ts
+++ class.ts
@@ -1,4 +1,8 @@
-// OOP --> making Object from Class
+/**
+ * OOP --> making instance from Class. Instance is a specific manifestation of class while
+ * object is variable of data structure consisting of key-value pair. where instance can
+ * have method from their class. Method is a function that is inside a class or object.
+ */
 
 // class Animal {
 //   name: string;
---
+++ inheritance.ts
@@ -0,0 +1,43 @@
+class Person {
+  name: string; //common properties
+  age: number; //common properties
+  address: string; //common properties
+
+  constructor(name: string, age: number, address: string) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+  //common properties
+  getSleep(numOfHours: number) {
+    console.log(`${this.name} ${numOfHours} ghonta ghumai`);
+  }
+}
+
+class Student extends Person {}
+class Teacher extends Person {
+  designation: string; //extra property
+
+  constructor(name: string, age: number, address: string, designation: string) {
+    super(name, age, address); // super --> constructor of parent class
+
+    this.designation = designation;
+  }
+
+  takesClass(numOfClass: number) {
+    console.log(`${this.name} ${numOfClass} ghonta class ney`);
+  }
+}
+
+const student1 = new Student("Mr. Fakibaz", 18, "bangaldesh");
+
+student1.getSleep(10);
+
+const teacher1 = new Teacher(
+  "Mr. smart teacher",
+  25,
+  "Bangaldesh",
+  "senior teacher",
+);
+
+teacher1.takesClass(4);
