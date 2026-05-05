4a583ce3b455848fce9487deea67dbc1f90cc0fb
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-05 14:48:34 +0600

    abstraction

---
+++ abstraction.ts
@@ -0,0 +1,49 @@
+/**
+ * abstraction --> idea pai. how it works? doesn't know
+ * abstraction can be implemented in 2 ways: 1. interface, 2. abstract class
+ */
+
+// interface MediaPlayer {
+//   play(): void;
+//   pause(): void;
+//   stop(): void;
+// }
+
+// // implementation
+// class MusicPlayer implements MediaPlayer {
+//   play() {
+//     console.log(`Playing Music`);
+//   }
+//   pause() {
+//     console.log(`Music Paused..`);
+//   }
+//   stop() {
+//     console.log(`Music stoped`);
+//   }
+// }
+
+// const ShafPlayer = new MusicPlayer();
+// ShafPlayer.play();
+
+// idea
+abstract class MediaPlayer {
+  abstract play(): void;
+  abstract pause(): void;
+  abstract stop(): void;
+}
+
+// implementation
+class ShafPlayer extends MediaPlayer {
+  play() {
+    console.log(`Playing Music`);
+  }
+  pause() {
+    console.log(`Music is Paused`);
+  }
+  stop() {
+    console.log(`Music is stoped`);
+  }
+}
+
+const shafPlayer = new ShafPlayer();
+shafPlayer.play();
